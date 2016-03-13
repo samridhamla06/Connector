1bde0ca8a7e3d2091d1d25c51ee5ef7e984b2c2c
Samridh Amla <samridhamla06@Samridhs-MacBook-Pro.local>
2016-03-14 03:48:52 +0530

    added some system files

--- app.js
+++ app.js
@@ -8,11 +8,8 @@ var authLoginObject = require(__dirname + '/routes/auth_login.js');
 var app = express();
 app.use(bodyParser.json());
 
-function tokenValidate() {
-  return true;
-}
 
-app.get('/authLogin',authLoginObject.authLogin,groupsObject.read);
+
 app.post('/authLogin',authLoginObject.authLogin,groupsObject.read);
 app.post('/register',registerObject.register);//middleware should check if username already exists.
 app.post('/login',loginObject.login);
--- routes/login.js
+++ routes/login.js
@@ -17,9 +17,11 @@ var validateCredentials = function(req,callback) {
      mongoClient.connect(mongoURL,function(err,db){
         if(err){
             console.log('couldnt connect to MongoDb');
+            callback(err,null);//as the END USER SHOULD BE NOTIFIED ABOUT THIS
         }else {
-            console.log('inside else......');
+            console.log('GOT CONNECTED TO MONGODB');
             var cursor = db.collection('All_Users').find({"username": req.body.username,"password":req.body.password});
+            //cursor.limit(1);
             cursor.toArray(callback);//should make another function
         }
     })
@@ -27,15 +29,17 @@ var validateCredentials = function(req,callback) {
 }
 
 exports.login = function(req,res){
+    console.log(req.body.username + ' tryin to log in');    //TENTATIVE########********^^^^^^@@@@@@@
 
-    validateCredentials(req,function(err){//IMP toArray() CALLBACK SENDS TWO PARAMETERS (err,record)
-        if(!err){
+    validateCredentials(req,function(err,record){//IMP toArray() CALLBACK SENDS TWO PARAMETERS (err,record)
+        console.log('No of records with '  + req.body.username +' is ' + record.length);
+        if(!err && record &&record.length > 0){//CAN CREATE A FUNCTION for THIS
             console.log('Valid UserName');
             signToken(req.body.username,60*60);
             console.log('the user ' + req.body.username + ' has logged in');
             res.send({"status":"Valid","token":token});
         }else {
-            res.send({"status":"Invalid","reason":"username or password is invalid"});
+            res.send({"status":"Invalid","token":null,"reason":null});
         }
     })
 }
