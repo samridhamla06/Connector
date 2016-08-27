2724655e521eb137f53f31885b1d75cecae0aef7
Samridh Amla <samridhamla06@Samridhs-MacBook-Pro.local>
2016-08-27 16:21:51 +0530

    last commit for this project :(

--- Server.js
+++ Server.js
@@ -4,5 +4,3 @@ var server = require('http').createServer(app);
 server.listen(8000,function(){
     console.log('Server running at port 8000');
 });
-
-//910f67a0-fb99-4130-b792-db88a47cc8df
--- app.js
+++ app.js
@@ -3,18 +3,17 @@ var bodyParser = require('body-parser');
 var registerObject = require(__dirname + '/routes/register.js');
 var groupsObject = require(__dirname + '/routes/groups.js');
 var loginObject = require(__dirname + '/routes/login.js');
-var authLoginObject = require(__dirname + '/routes/auth_login.js');
 var usersObject = require(__dirname + '/routes/users.js');
 var userDescriptionObject = require(__dirname + '/routes/user_description.js');
 var checkAuthorizationObject = require(__dirname + '/routes/checkAuthorization.js');
 var addGroupObject = require(__dirname + '/routes/addGroup.js');
 var joinGroupObject = require(__dirname + '/routes/joinGroup.js');
-
-
 var app = express();
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get('/',function(req,res){
     res.send({"status":"hello"});
